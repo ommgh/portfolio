eb55a235867a0257ecfd3d6e188d06a00e09f0f9
Om Mishra <om.works01@gmail.com>
2025-02-26 00:44:12 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provier";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           <main className=" flex-grow ">{children}</main>
         </ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
